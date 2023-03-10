e09388cdcbf7ebcf736c67795833b04905414d8f
VirajJaneja <virajjaneja@gmail.com>
2023-03-10 11:45:17 -0500

    started to work on block 1

---
+++ src/main/java/com/vrage/odinslegacy/blocks/BlockBase.java
@@ -0,0 +1,14 @@
+package com.vrage.odinslegacy.blocks;
+
+import com.vrage.odinslegacy.OdinsLegacy;
+import jdk.nashorn.internal.ir.Block;
+import jdk.nashorn.internal.ir.Statement;
+import net.minecraft.item.Item;
+
+public class BlockBase extends Block {
+
+    public BlockBase() {
+        super(new Block().grou);
+
+    }
+}
--- src/main/java/com/vrage/odinslegacy/blocks/OLblockRegistry.java
+++ src/main/java/com/vrage/odinslegacy/blocks/OLblockRegistry.java
@@ -1,4 +1,30 @@
 package com.vrage.odinslegacy.blocks;
 
+import com.vrage.odinslegacy.OdinsLegacy;
+import com.vrage.odinslegacy.items.ItemBase;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class OLblockRegistry {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, OdinsLegacy.MOD_ID);
+
+    public static void register(IEventBus eventBus){
+        BLOCKS.register(eventBus);
+    }
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP)));
+    }
+
+    public static final RegistryObject<Item> NORDIC_STEEL = BLOCKS.register("midgardian_steel_block", ItemBase::new);
+
 }
